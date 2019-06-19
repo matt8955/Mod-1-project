fd19bcc5f0e16c0a7ebd507d9a0ad7efa7dcdab4
Matthew Wasserman <matt8955@gmail.com>
2019-06-19 16:00:02 -0400

    updated function to copy

--- functions.py
+++ functions.py
@@ -1,22 +1,29 @@
+import pandas as pd
 import numpy as np
-#clear outliers
-def clean_outliers(df):
+def remove_outliers(df):  
+    pd_copy = pd.DataFrame(df, copy=True)
     for col in df.columns:
-        q1 = df['{}'.format(col)].quantile(0.25)
-        q3 = df['{}'.format(col)].quantile(0.75)
-        iqr = q3-q1 #Interquartile range
-        fence_low  = q1-1.5*iqr
-        fence_high = q3+1.5*iqr
-        df_out = df.loc[(df['{}'.format(col)] > fence_low) & (df['{}'.format(col)] < fence_high)]
-        return df_out
+        try:
+            q1 = df[col].quantile(0.25)
+            q3 = df[col].quantile(0.75)
+            iqr = q3-q1 #Interquartile range
+            fence_low  = q1-1.5*iqr
+            fence_high = q3+1.5*iqr
+            pd_copy.loc[(pd_copy[col] < fence_low) | (pd_copy[col] > fence_high),col] = np.nan
+        
+        except:
+            pd_copy[col] == df[col]
     
+    return pd_copy
+
 #take in list of columns in def and log transform
 def log_transform(df, cols):
     ''' takes in dataframe and cols to log transfrom and returns 
         the dataset with the log transformed columns dropped the regular
         col'''
+    df_copy = df.copy()
     for col in cols:
-        df[col] = np.log(df[col])
-        df['log_{}'.format(col)] = df[col]
-        df.drop(col, axis=1, inplace=True)
-    return df
+        df_copy[col] = np.log(df_copy[col])
+        df_copy['log_{}'.format(col)] = df_copy[col]
+        df_copy.drop(col, axis=1, inplace=True)
+    return df_copy
