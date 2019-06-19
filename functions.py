3e6b1d7c225e36583f49a1200d734c2cd27774a5
Minna Fingerhood <mfingerhood@gmail.com>
2019-06-19 15:51:23 -0400

    updated with outliers and cleaning columns

--- functions.py
+++ functions.py
@@ -1,16 +1,46 @@
 import numpy as np
-#clear outliers
-def clean_outliers(df):
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt 
+import statsmodels.formula.api as smf
+%matplotlib inline
+
+
+#remove outliers
+def remove_outliers(df):  
+    pd_copy = pd.DataFrame(df, copy=True)
     for col in df.columns:
-        q1 = df['{}'.format(col)].quantile(0.25)
-        q3 = df['{}'.format(col)].quantile(0.75)
-        iqr = q3-q1 #Interquartile range
-        fence_low  = q1-1.5*iqr
-        fence_high = q3+1.5*iqr
-        df_out = df.loc[(df['{}'.format(col)] > fence_low) & (df['{}'.format(col)] < fence_high)]
-        return df_out
+        try:
+            q1 = df[col].quantile(0.25)
+            q3 = df[col].quantile(0.75)
+            iqr = q3-q1 #Interquartile range
+            fence_low  = q1-1.5*iqr
+            fence_high = q3+1.5*iqr
+            pd_copy.loc[(pd_copy[col] < fence_low) | (pd_copy[col] > fence_high),col] = np.nan
+        
+        except:
+            pd_copy[col] == df[col]
     
-#take in list of columns in def and log transform
+    return pd_copy
+    
+df = remove_outliers(df)  
+
+#clean data:
+def clean_data(df):
+    #check duplicates... BUT duplicates are a result of the same house being sold a second or third time 
+    #yr_renovated column has both nan and 0.0 filler values... change all to nan so 0.0 doesn't skew data
+    df['yr_renovated'] = df['yr_renovated'].replace(0.0, np.nan)
+
+    df['waterfront'] = df['waterfront'].fillna(0.0)
+
+    #sqft_basement in string format, convert and replace 0.0 with null to find median without skewed outliers
+    df['sqft_basement'] = pd.to_numeric(df['sqft_basement'], errors='coerce')
+    df['sqft_basement'] = df['sqft_basement'].replace(0.0, np.nan)
+    df['sqft_basement'] = df['sqft_basement'].fillna(df['sqft_basement'].median())
+
+    return df
+    
+#take in list of columns in def and log transform so that we only transform specified columns 
 def log_transform(df, cols):
     ''' takes in dataframe and cols to log transfrom and returns 
         the dataset with the log transformed columns dropped the regular
