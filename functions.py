8c280e229168ee4c176ab5d057630427e87a0a97
Minna Fingerhood <mfingerhood@gmail.com>
2019-06-20 16:48:04 -0400

    walk-through and final functions

--- functions.py
+++ functions.py
@@ -6,6 +6,7 @@ import statsmodels.formula.api as smf
 import statsmodels.api as sm
 import scipy as sp
 
+
 #remove outliers
 def remove_outliers(df, cols):  
     ''' takes in df and cols we want to remove outliers from
@@ -19,11 +20,14 @@ def remove_outliers(df, cols):
             fence_low  = q1-1.5*iqr
             fence_high = q3+1.5*iqr
             pd_copy.loc[(pd_copy[col] < fence_low) | (pd_copy[col] > fence_high),col] = np.nan
+            df = pd_copy
         
         except:
             pd_copy[col] == df[col]
-    
-    return pd_copy
+            
+    return df
+
+
 
 #clean data:
 def clean_data(df):
@@ -32,27 +36,29 @@ def clean_data(df):
 
     #yr_renovated column has both nan and 0.0 filler values... change all to nan so 0.0 doesn't skew data
     df['yr_renovated'] = df['yr_renovated'].replace(0.0, np.nan)
-
     df['waterfront'] = df['waterfront'].fillna(0.0)
+    df['view'] = df['view'].fillna(0.0)
+    df['sqft_basement'] = df['sqft_basement'].replace('?', np.nan)
+    
     return df
 
 
 
-def replace_null_w_median(list_columns):
+def replace_null_w_median(list_columns, df):
+    df_copy = df.copy()
     for col in list_columns:
         if col in df.columns: 
-            df[col] = pd.to_numeric(df[col], errors='coerce')
-            df[col] = df[col].replace(0.0, np.nan)
-            df[col] = df[col].fillna(df[col].median()) 
+            df_copy[col] = pd.to_numeric(df_copy[col], errors='coerce')
+            df_copy[col] = df_copy[col].replace(0.0, np.nan)
+            df_copy[col] = df_copy[col].fillna(df[col].median())   
                    
         else: 
             return print('inputted columns not in dataframe-- try again')
         
-        return 
-
+        return df_copy
+        
 
     
-import numpy as np #this is imported above. Should we delete?
 
 def log_transform(df, cols):
     ''' takes in dataframe and cols to log transfrom and returns 
@@ -69,15 +75,59 @@ def log_transform(df, cols):
 def scatter_one_vs_all(df, column):
     ''' a data frame and one column and plots scatters for all cols and
     against one var only works on current data sets # of features'''
+    df_copy = df.copy()
     fig, ax = plt.subplots(5,4, figsize=(30,30))
-    l = list(df.columns)
+    l = list(df_copy.columns)
     l.remove(column)
     i = 0 #to track col index
     for m in range(5):
         for n in range(4):
-            sns.scatterplot(x=l[i],y=column,data=df,ax=ax[m][n])
+            sns.scatterplot(x=l[i],y=column,data=df_copy,ax=ax[m][n])
             i += 1
     return 0
+
+
+def regression_plot(df, column):
+    '''takes in df and tries to graph every independent var against the 
+    column variable if seaborn cant plot it, revert to scatter'''
+    df_copy = df.copy()
+    fig, ax = plt.subplots(5,4, figsize=(30,30))
+    l = list(df_copy.columns)
+    l.remove(column)
+    i = 0 #to track col index
+    for m in range(5):
+        for n in range(4):
+            try:
+                sns.regplot(x=l[i],y=column,data=df_copy,ax=ax[m][n])
+                i += 1
+            except:
+                sns.scatterplot(x=l[i],y=column,data=df_copy,ax=ax[m][n])
+                i += 1
+    return 
+
+
+
+def qq_plot(depend, df):
+    df_copy = df.copy()
+    features = list(df.columns)
+    features.remove(depend) #make col of all features to loop across
+    fig, ax = plt.subplots(5,4, figsize=(30,30))
+    i=0
+    for m in range(5):
+        for n in range(4):
+            f = '{}~{}'.format(depend, features[i])
+            model = smf.ols(formula=f, data=df_copy).fit()
+            resid1 = model.resid
+            sm.qqplot(resid1, dist=sp.stats.norm, line='45', fit=True, ax=ax[m][n])
+            ax[m][n].set_title('{}'.format(features[i]))
+            i += 1
+    return 
+
+
+
+def histogram(df):
+    return df.hist(bins=50, figsize=(20,15))
+
 
 def partial_regress(depend, df):
     features = list(df.columns)
@@ -113,41 +163,40 @@ def jarque_bera(depend, df):
 #     return list(zip(name, test))  
 
 
-
 from sklearn.feature_selection import RFE 
 from sklearn.linear_model import LinearRegression
 
 
-
-#replace_null_w_median(['bedrooms', 'bathrooms', 'view'])
-
-
-
-
-
-def test_predictors(list_of_features):
+def test_predictors(list_of_features, df, num_pred):
+    '''input list of features, df, numbers of predictors you want to compare against log_price'''
     predictors = df.reindex(columns=list_of_features)
 
     linreg = LinearRegression()
-    selector = RFE(linreg, n_features_to_select = 2)
+    selector = RFE(linreg, n_features_to_select = num_pred)
     selector = selector.fit(predictors, df['log_price'])
 
-    selector_list = selector.support_
+    selector_list = selector.ranking_
     answer_list = []
+    estimators = selector.estimator_
+
     
     for i in range(0,len(selector_list)):
          answer_list.append(f'{list_of_features[i]} - {selector_list[i]}')
     
-    return answer_list
+    return (answer_list, estimators.coef_, estimators.intercept_)
+
+
+from statsmodels.formula.api import ols
+
+def create_model():
+    f = 'log_price~ log_sqft_living + waterfront + grade + condition + log_sqft_living15 + view + yr_built + zipcode'
+    model = ols(formula = f, data = df).fit()
+    return model.summary()
 
 
 
 
 
-
-
-
-
-
+##
 
 
